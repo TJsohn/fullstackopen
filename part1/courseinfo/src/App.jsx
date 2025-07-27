c0c8db37f88fc590b310f3f090851078b13f85fc
TJsohn <sohntaejeong@gmail.com>
2025-07-27 22:27:35 +0300

    Add solutions for 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,10 +34,10 @@ const Total = (props) => {
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
     {
       name: 'Using props to pass data',
       exercises: 7
