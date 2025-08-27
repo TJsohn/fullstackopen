b00e968b3a8d52b166e3f7c7371fae354552b029
TJsohn <sohntaejeong@gmail.com>
2025-08-27 22:32:04 +0300

    add solutions for exercise 2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </div>
+)
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((s, p) => s + p.exercises, 0)
+  return <p><strong>total of {sum} exercises</strong></p>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
